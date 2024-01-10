79e139a0a3caea0b49ac30d8388dc67d2d8ef095
Emmanuel Owolabi <owoemma59@gmail.com>
2024-01-10 09:38:47 +0100

    adding size boxes

--- src/components/ImageBackgroundInfo.tsx
+++ src/components/ImageBackgroundInfo.tsx
@@ -8,7 +8,14 @@ import {
 } from 'react-native';
 import React from 'react';
 import GradientBgIcon from './GradientBgIcon';
-import {COLORS, FONTSIZE, SPACING} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './CustomIcon';
 
 interface ImageBackgroundProps {
   enableBackHanlder: boolean;
@@ -48,14 +55,14 @@ const ImageBackgroundInfo: React.FC<ImageBackgroundProps> = ({
         style={styles.ItemBackgroundImage}>
         {enableBackHanlder ? (
           <View style={styles.BackButtonContainer}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={BackHandler}>
               <GradientBgIcon
                 name="left"
                 color={COLORS.primaryLightGreyHex}
                 size={FONTSIZE.size_16}
               />
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={ToggleFavourite}>
               <GradientBgIcon
                 name="like"
                 color={
@@ -78,6 +85,61 @@ const ImageBackgroundInfo: React.FC<ImageBackgroundProps> = ({
             </TouchableOpacity>
           </View>
         )}
+        <View style={styles.ImageInfoOuterContainer}>
+          <View style={styles.ImageInfoInnerContainer}>
+            <View style={styles.InfoContainerRow}>
+              <View>
+                <Text style={styles.ItemTitleText}>{name}</Text>
+                <Text style={styles.ItemSubtitleText}>
+                  {special_ingredient}
+                </Text>
+              </View>
+              <View style={styles.ItemPropertiesContainer}>
+                <View style={styles.ProperFirst}>
+                  <CustomIcon
+                    name={name === 'Bean' ? 'bean' : 'beans'}
+                    size={name === 'Bean' ? FONTSIZE.size_18 : FONTSIZE.size_24}
+                    color={COLORS.primaryOrangeHex}
+                  />
+                  <Text
+                    style={[
+                      styles.PropertyTextFirst,
+                      {
+                        marginTop:
+                          type == 'Bean'
+                            ? SPACING.space_4 + SPACING.space_2
+                            : 0,
+                      },
+                    ]}>
+                    {type}
+                  </Text>
+                </View>
+                <View style={styles.ProperFirst}>
+                  <CustomIcon
+                    name={type === 'Bean' ? 'location' : 'drop'}
+                    size={FONTSIZE.size_16}
+                    color={COLORS.primaryOrangeHex}
+                  />
+                  <Text style={styles.PropertyTextLast}>{ingredients}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.InfoContainerRow}>
+              <View style={styles.RatingContainer}>
+                <CustomIcon
+                  name="star"
+                  color={COLORS.primaryOrangeHex}
+                  size={FONTSIZE.size_20}
+                />
+                <Text style={styles.RatingText}>{average_rating}</Text>
+                <Text style={styles.RatingCountText}>{ratings_count}</Text>
+              </View>
+              <View style={styles.RoastedContainer}>
+                <Text style={styles.RoastedText}>{roasted}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -97,5 +159,88 @@ const styles = StyleSheet.create({
     padding: SPACING.space_30,
     alignItems: 'center',
   },
-  WithoutBackButtonContainer: {},
+  WithoutBackButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: SPACING.space_30,
+    alignItems: 'center',
+  },
+  ImageInfoOuterContainer: {
+    paddingVertical: SPACING.space_24,
+    paddingHorizontal: SPACING.space_30,
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopLeftRadius: BORDERRADIUS.radius_20 * 2,
+    borderTopRightRadius: BORDERRADIUS.radius_20 * 2,
+  },
+  ImageInfoInnerContainer: {
+    justifyContent: 'space-between',
+    gap: SPACING.space_15,
+  },
+  InfoContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  ItemTitleText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.primaryWhiteHex,
+  },
+  ItemSubtitleText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  ItemPropertiesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_20,
+  },
+  ProperFirst: {
+    height: 55,
+    width: 55,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  PropertyTextFirst: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+  },
+  PropertyTextLast: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+    marginTop: SPACING.space_4 + SPACING.space_2,
+  },
+  RatingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_10,
+  },
+  RatingText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+  RatingCountText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  RoastedContainer: {
+    height: 55,
+    width: 55 * 2 + SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  RoastedText: {
+    color: COLORS.primaryWhiteHex,
+    fontSize: FONTSIZE.size_12,
+    fontFamily: FONTFAMILY.poppins_regular,
+  },
 });
--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,7 +1,21 @@
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import {useStore} from '../store/store';
-import {COLORS} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
 
 const DetailsScreen = ({navigation, route}: any) => {
@@ -11,6 +25,20 @@ const DetailsScreen = ({navigation, route}: any) => {
   console.log(route.params);
   console.log(itemOfIndex);
 
+  const {addToFavoriteList, deleteFromFavoriteList} = useStore(
+    (state: any) => state,
+  );
+
+  const [price, setPrice] = useState(itemOfIndex.prices[0]);
+  const [fullDesc, setFullDesc] = useState(false);
+
+  const toggleFavourite = (favorite: boolean, type: string, id: string) => {
+    if (favorite) {
+      deleteFromFavoriteList(type, id);
+    } else {
+      addToFavoriteList(type, id);
+    }
+  };
   return (
     <SafeAreaView style={styles.ScreenContainer}>
       <ScrollView
@@ -28,9 +56,68 @@ const DetailsScreen = ({navigation, route}: any) => {
           average_rating={itemOfIndex.average_rating}
           ratings_count={itemOfIndex.ratings_count}
           roasted={itemOfIndex.roasted}
-          BackHandler={() => {}}
-          ToggleFavourite={() => {}}
+          BackHandler={() => {
+            navigation.goBack();
+          }}
+          ToggleFavourite={() => {
+            toggleFavourite(
+              itemOfIndex.favourite,
+              itemOfIndex.type,
+              itemOfIndex.id,
+            );
+          }}
         />
+        <View style={styles.FooterInfoArea}>
+          <Text style={styles.InfoTitle}>Description</Text>
+          {fullDesc ? (
+            <TouchableWithoutFeedback
+              onPress={() => setFullDesc(prev => !prev)}>
+              <Text style={styles.DescriptionText}>
+                {itemOfIndex.description}
+              </Text>
+            </TouchableWithoutFeedback>
+          ) : (
+            <TouchableWithoutFeedback
+              onPress={() => setFullDesc(prev => !prev)}>
+              <Text numberOfLines={3} style={styles.DescriptionText}>
+                {itemOfIndex.description}
+              </Text>
+            </TouchableWithoutFeedback>
+          )}
+          <Text style={styles.InfoTitle}>Size</Text>
+          <View style={styles.SizeOuterContainer}>
+            {itemOfIndex.prices.map((data: any) => {
+              <TouchableOpacity
+                key={data.size}
+                style={[
+                  styles.SizeBox,
+                  {
+                    borderColor:
+                      data.size == price.size
+                        ? COLORS.primaryOrangeHex
+                        : COLORS.primaryDarkGreyHex,
+                  },
+                ]}>
+                <Text
+                  style={[
+                    styles.SizeText,
+                    {
+                      fontSize:
+                        itemOfIndex.type == 'bean'
+                          ? FONTSIZE.size_14
+                          : FONTSIZE.size_16,
+                      color:
+                        data.size == price.size
+                          ? COLORS.primaryOrangeHex
+                          : COLORS.primaryLightGreyHex,
+                    },
+                  ]}>
+                  {data.size ? data.size : 'N/A'}
+                </Text>
+              </TouchableOpacity>;
+            })}
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -44,4 +131,34 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.primaryBlackHex,
   },
   ScrollViewFlex: {flexGrow: 1},
+  FooterInfoArea: {padding: SPACING.space_20},
+  InfoTitle: {
+    fontSize: FONTSIZE.size_16,
+    fontFamily: FONTFAMILY.poppins_semibold,
+    color: COLORS.primaryWhiteHex,
+    marginBottom: SPACING.space_10,
+  },
+  DescriptionText: {
+    fontSize: FONTSIZE.size_14,
+    fontFamily: FONTFAMILY.poppins_regular,
+    color: COLORS.primaryWhiteHex,
+    marginBottom: SPACING.space_30,
+    letterSpacing: 0.5,
+  },
+  SizeOuterContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: SPACING.space_20,
+  },
+  SizeBox: {
+    flex: 1,
+    backgroundColor: COLORS.primaryDarkGreyHex,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: SPACING.space_24 * 2,
+    borderRadius: BORDERRADIUS.radius_10,
+    borderWidth: 2,
+  },
+  SizeText: {},
 });
